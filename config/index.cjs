130e278a35b533de5260892556629dd706cf307d
new-hello <118009751+new-hello@users.noreply.github.com>
2022-11-15 23:39:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -104,7 +104,7 @@ const USER_CONFIG = {
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'Zv0NC1YS-cHkcSqs_my9xDMkZzJHp5cU_rg_jvw9i4s',
+      useTemplateId: 'EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
