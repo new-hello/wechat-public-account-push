4bd0095d857c09ae2ea19ad007c2bdc1daed6c95
new-hello <118009751+new-hello@users.noreply.github.com>
2022-11-15 21:24:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -66,7 +66,7 @@ const USER_CONFIG = {
   
   TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: '65687576542844d3796c6cf07a78cd47',
+    key: '',
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
