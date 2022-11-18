ed3edbca24d608f5d02bb3a8a442976e468653d2
new-hello <118009751+new-hello@users.noreply.github.com>
2022-11-18 23:47:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -4,17 +4,38 @@ const USER_CONFIG = {
    * 基本配置
    */
   USE_PASSAGE: 'wechat-test', 
+  
   APP_ID: '',
+  
   APP_SECRET: '',
   
   // 功能开关,打开：true，关闭：false
   SWITCH: {
+    
     weather: true,
+    
     holidaytts: false,
+    
     CIBA: true,
+    
+    oneTalk: false,
+    
+    earthyLoveWords: false,
+    
+    momentCopyrighting: false,
+    
+    poisonChickenSoup: false,
+    
+    poetry: false,
+    
     horoscope: true,
+    
     birthdayMessage: true,
+    
+    courseSchedule: true,
+    
     NOTICE:true,
+    
   },
   
   IS_SHOW_COLOR: true,
@@ -22,25 +43,37 @@ const USER_CONFIG = {
   USERS: [
     {
       name: '露露宝贝',
+      
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
+      
       useTemplateId: '	EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc',
+      
       province: '四川',
+      
       city: '内江',
+      
       horoscopeDate: '04-21',
+      
       horoscopeDateType: '今日',
+      
       
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         //{type: '*生日', name: '露露宝贝', year: '2001', date: '04-21',},
+        
         //{type: '节日', name: '无', year: '2020', date: '09-03',},
         
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         { type: '生日', name: '露露宝贝', year: '2001', date: '04-21',},
+        
         { type: '节日', name: '除夕', year: '2023', date: '01-21',}
+        
       ],
       
       customizedDateList: [
+        
         { keyword: 'love_day', date: '2022-05-22' },
+        
       ],
     },
   ],
@@ -50,7 +83,9 @@ const USER_CONFIG = {
   CALLBACK_USERS: [
     {
       name: '自己',
+      
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
+      ]
     }
   ],
 }
