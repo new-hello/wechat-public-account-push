274d6f6d41e594e9ac74c43e22f61ff9f74ba79a
new-hello <118009751+new-hello@users.noreply.github.com>
2022-11-19 00:06:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -3,60 +3,95 @@ const USER_CONFIG = {
   /**
    * 基本配置
    */
-  USE_PASSAGE: 'wechat-test', 
   
+  // 使用的推送通道：['push-deer', 'wechat-test', 'server-chan', 'push-plus']
+  // 默认使用 【微信测试号】
+  // 使用【pushDeer】请填写 push-deer
+  // 使用【微信测试号】请填写 wechat-test
+  // 使用【方糖服务号】请填写 server-chan
+  // 使用【pushplus推送加服务号】请填写 push-plus
+  USE_PASSAGE: 'wechat-test',
+  
+  // 使用微信测试号时才需要填写：公众号APP_ID
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
   APP_ID: '',
-  
+
+  // 使用微信测试号时才需要填写：公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '',
   
+  // 为了避免推送服务器误将脚本列为恶意推送脚本，可设置每分钟脚本最大推送数
+  // 每分钟脚本最大推送数，超过此数将会休眠1分钟后再发送剩余消息，不填则默认为5
+  // 此项不建议随意修改
+  MAX_PUSH_ONE_MINUTE: 5,
+  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
+  SLEEP_TIME: 65000,
+
   // 功能开关,打开：true，关闭：false
   SWITCH: {
-    
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     weather: true,
-    
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     holidaytts: false,
-    
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     CIBA: true,
     
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     oneTalk: false,
     
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     earthyLoveWords: false,
     
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     momentCopyrighting: false,
     
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     poisonChickenSoup: false,
     
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     poetry: false,
-    
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     horoscope: true,
-    
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     birthdayMessage: true,
-    
-    courseSchedule: true,
-    
-    NOTICE:true,
-    
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
   },
   
   IS_SHOW_COLOR: true,
   
-  USERS: [
+ USERS: [
     {
+      // 想要发送的人的名字
       name: '露露宝贝',
-      
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
-      
-      useTemplateId: '	EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc',
-      
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc',
+      // 所在省份或城市，也可以不填
       province: '四川',
-      
+      // 所在城市或县区
       city: '内江',
-      
+      // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '04-21',
-      
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
-      
-      
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         //{type: '*生日', name: '露露宝贝', year: '2001', date: '04-21',},
@@ -69,25 +104,36 @@ const USER_CONFIG = {
         { type: '节日', name: '除夕', year: '2023', date: '01-21',}
         
       ],
-      
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
-        
+        // 在一起的日子
         { keyword: 'love_day', date: '2022-05-22' },
-        
+      
       ],
     },
   ],
   
+  /**
+   * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
+   */
+
+  // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
   CALLBACK_TEMPLATE_ID: 'M6sw_D_e-tCFmh0S-7cPzCuVMHLouxgoSWTIA-orauY',
-  
+
+  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
+      // 一般都填自己
       name: '自己',
-      
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
-      
     }
+    // 你可以不断按格式往下增加
+    // ...
   ],
+
 }
 
 
