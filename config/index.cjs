cbb98b1847d0fd7422e71800cc57e7047d8c2a51
new-hello <118009751+new-hello@users.noreply.github.com>
2022-11-21 10:44:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -75,7 +75,7 @@ const USER_CONFIG = {
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc	',
+      useTemplateId: 'EJWi2QHs0ZMrjjCBvBoEHL_3OH4UkoiNge2UAFG7rmc',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
@@ -121,7 +121,7 @@ const USER_CONFIG = {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
+      id: 'M6sw_D_e-tCFmh0S-7cPzCuVMHLouxgoSWTIA-orauY',
     }
     // 你可以不断按格式往下增加
     // ...
