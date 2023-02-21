5f80f33165197afde469ef53b1cd500fccdf4b51
new-hello <118009751+new-hello@users.noreply.github.com>
2023-02-21 15:12:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -3,157 +3,75 @@ const USER_CONFIG = {
 
   USE_PASSAGE: 'wechat-test',
  
- // 使用微信测试号时才需要填写：公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_ID: '',
 
-  // 使用微信测试号时才需要填写：公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '',
   
-  // 为了避免推送服务器误将脚本列为恶意推送脚本，可设置每分钟脚本最大推送数
-  // 每分钟脚本最大推送数，超过此数将会休眠1分钟后再发送剩余消息，不填则默认为5
-  // 此项不建议随意修改
-  MAX_PUSH_ONE_MINUTE: 5,
-  // 配合MAX_PUSH_ONE_MINUTE使用，休眠<SLEEP_TIME>毫秒后再发送剩余消息，不填则默认为65000
-  SLEEP_TIME: 65000,
-
   // 功能开关,打开：true，关闭：false
   SWITCH: {
     /** 每日天气 */
-    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: true,
+    weather: false,
 
     /** 节假日 */
-    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    holidaytts: true,
+    holidaytts: false,
 
     /** 每日N句 */
     // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    CIBA: true,
+    CIBA: false,
     
     // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    oneTalk: true,
+    oneTalk: false,
     
     // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    earthyLoveWords: true,
+    earthyLoveWords: false,
     
     // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    momentCopyrighting: true,
+    momentCopyrighting: false,
     
     // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poisonChickenSoup: true,
+    poisonChickenSoup: false,
     
     // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    poetry: true,
+    poetry: false,
 
     /** 星座运势 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    horoscope: true,
+    horoscope: false,
   
     /** 生日消息和节日消息 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    birthdayMessage: true,
+    birthdayMessage: false,
   
     /** 学生课表 */
     // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
     courseSchedule: true,
   },
   
-  TIAN_API: {
-    // 天行API KEY，如果使用天行API则需要填写此项
-    key: '',
-    
-    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
-    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: true,
-  
-    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    eveningGreeting: true,
-  
-    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: 3,
-  
-    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    networkHot: 3,
-  
-    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
-    networkHotType: 'default',
-  },
-  
-  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
-  
-  /** 每日一言 */
-  // 每日一言的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '',
-  
-
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '',
-      // 所在省份或城市，也可以不填
-      province: '惠州',
-      // 所在城市或县区
-      city: '惠阳',
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      name: '本人',
+      
+      id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
+      
+      useTemplateId: 'Urremeyf7pCM2sfDIgwst0n5lQpPW8sA1LKPBiy4avY',
+
+      province: '四川',
+
+      city: '达州',
+
+      horoscopeDate: '10-14',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://wangxinleo.cn',
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
-        },
-        {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
-        }
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-        // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
-      ],
+      
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
       // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
       // 如果courseSchedule是一个对象（如下面所示）
       courseSchedule: {
-        // 单双周的基准
-        benchmark: {
-          // 这里设置一个日期，用来作为判断课表是否单双周的依据
-          date: '2022-09-23',
-          // 该日期是否为单周
-          isOdd: true
-        },
         // 课表
         courses: {
           // 单周课表
@@ -161,77 +79,40 @@ const USER_CONFIG = {
           odd: [
             // 例子，周一的课表
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              '08:30 -10:00 微信小程序开发 204A',
+              '10:20-11:50 微信小程序开发 五机房',
+              '14:40-16:10 大数据技术与应用 407A',
+              '16:20-17:50 大数据技术与应用 一机房'
             ],
             // 周二
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              '08:30-10:00 交互界面设计 112B',
+              '10:20-11:50 交互界面设计 二机房'
             ],
             // 周三
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              '08:30-10:00 影视特效制作 208A',
+              '10:20-11:50 影视特效制作 五机房'
             ],
             // 周四
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              '08:30-10:00 虚拟现实应用设计 人工智能实训室2',
+              '10:20-11:50 虚拟现实应用设计 人工智能实训室2'
             ],
             // 周五
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
-            ],
-            // 周六
-            [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
-            ],
-            // 周日
-            [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              '08:30-10:00 广告创意 201A',
+              '10:20-11:50 广告创意 一机房'
             ]
           ],
-          // 双周课表
-          even: [
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            []
-          ]
-        }
-      },
-    },
-    {
-      name: '老婆1',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
-    }
-    // 你可以不断按格式往下增加
-    // ...
-  ],
-  
+        },
   /**
    * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
    */
 
   // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
-  CALLBACK_TEMPLATE_ID: 'M6sw_D_e-tCFmh0S-7cPzCuVMHLouxgoSWTIA-orauY',
+  CALLBACK_TEMPLATE_ID: '	uDGKSdrCUyi2Qh3Dz9ysjhVmwZz3KHkbHgThr1OKnEs',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
