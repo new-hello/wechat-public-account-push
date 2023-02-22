69a957c56ad397665abdc7320294737126276a52
new-hello <118009751+new-hello@users.noreply.github.com>
2023-02-22 20:44:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,11 +57,11 @@ const USER_CONFIG = {
       id: 'oHkJg6RMxKEg0nWPLzYj1j2oLvDI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'wZ9zR_JViFEs9JqtCgHKP7Yc6Kc--6CWWyIPqivVN_0	',
+      useTemplateId: 'wZ9zR_JViFEs9JqtCgHKP7Yc6Kc--6CWWyIPqivVN_0',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
-      city: '达州',
+      city: '',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '10-14',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
